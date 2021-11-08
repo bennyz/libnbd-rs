fee4f183de196ff104c349778d0e4ccab021b931
Benny Zlotnik <bzlotnik@protonmail.com>
2021-11-08 21:55:06 +0200

    basic sanity test

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,26 @@
 #![allow(non_snake_case)]
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+#[cfg(test)]
+mod test {
+    use std::ffi::{CStr, CString};
+
+    use super::*;
+
+    #[test]
+    fn test() {
+        unsafe {
+            let handle = nbd_create();
+
+            let c_str = CString::new("hello").unwrap();
+            nbd_set_handle_name(handle, c_str.as_ptr());
+            assert_eq!(
+                CStr::from_ptr(nbd_get_handle_name(handle))
+                    .to_str()
+                    .unwrap(),
+                "hello"
+            );
+        }
+    }
+}
